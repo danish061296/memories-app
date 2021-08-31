b8ee3dd09573d1824700db23be6f0302f2070968
Danish Siddqiui <dsiddiqui@mail.sfsu.edu>
2021-08-31 14:36:28 -0700

    Deleted src dir

--- src/constants/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const CREATE_POST = 'CREATE_POST';
-export const UPDATE_POST = 'UPDATE_POST';
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const DELETE_POST = 'DELETE_POST';
-export const LIKE_POST = 'LIKE_POST';
